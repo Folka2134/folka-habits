5e705b0b08916d92c3ac6800b5abe546ddfc5e54
folka2134 <folka2134@gmail.com>
2025-04-10 16:07:24 +0100

    refactor: Get total minutes for session function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Subject } from "./subjects"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function GetTotalMinutes(subject: Subject) {
+  let totalMinutes = 0
+  subject.sessions.forEach((session) => {
+    totalMinutes += (session.inputMinutes + session.outputMinutes)
+  })
+  return totalMinutes
 }
